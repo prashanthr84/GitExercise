c25d86034e0dca7d6edb8a823d07b7bf8c2be635
Rao <Prashanth.Rao@philips.com>
2022-04-29 22:05:11 +0530

    Added interfae and skeleton for multiplication

--- Source/CalculatorLibrary/CalculatorService.cs
+++ Source/CalculatorLibrary/CalculatorService.cs
@@ -5,13 +5,24 @@ namespace CalculatorLibrary
     public class CalculatorService
     {
         private readonly IAdditionPerformer performer;
+        private readonly IMultiplicationPerformer multiplicationPerformer;
 
-        public CalculatorService(IAdditionPerformer performer) {
+        public CalculatorService(
+            IAdditionPerformer performer,
+            IMultiplicationPerformer multiplicationPerformer
+        )
+        {
             this.performer = performer;
+            this.multiplicationPerformer = multiplicationPerformer;
         }
 
         public int Add(int num1, int num2) {
             return performer.Perform(num1, num2);
+        }
+
+        public int Multiply(int num1, int num2)
+        {
+            return multiplicationPerformer.Perform(num1, num2);
         }
     }
 }
