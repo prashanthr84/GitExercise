b421b943be7aaa443aad5faf6771f918e610ad33
Rao <Prashanth.Rao@philips.com>
2022-04-29 22:19:43 +0530

    renamed performer to additionperformer

--- Source/CalculatorLibrary/CalculatorService.cs
+++ Source/CalculatorLibrary/CalculatorService.cs
@@ -4,23 +4,23 @@ namespace CalculatorLibrary
 {
     public class CalculatorService
     {
-        private readonly IAdditionPerformer performer;
+        private readonly IAdditionPerformer additionPerformer;
 
         private readonly ISubtractionPerformer subtractionPerformer;
         private readonly IMultiplicationPerformer multiplicationPerformer;
 
         public CalculatorService(
-            IAdditionPerformer performer,
+            IAdditionPerformer additionPerformer,
             ISubtractionPerformer subtractionPerformer,
             IMultiplicationPerformer multiplicationPerformer
         ) {
-            this.performer = performer;
+            this.additionPerformer = additionPerformer;
             this.subtractionPerformer = subtractionPerformer;
             this.multiplicationPerformer = multiplicationPerformer;
         }
 
         public int Add(int num1, int num2) {
-            return performer.Perform(num1, num2);
+            return additionPerformer.Perform(num1, num2);
         }
 
         public int Multiply(int num1, int num2)
