ce9f9ee09974c01ea5b7f94ea717d2e72aeeece4
Rao <Prashanth.Rao@philips.com>
2022-04-29 22:01:46 +0530

    Supports subtraction

--- Source/CalculatorLibrary/CalculatorService.cs
+++ Source/CalculatorLibrary/CalculatorService.cs
@@ -5,13 +5,22 @@ namespace CalculatorLibrary
     public class CalculatorService
     {
         private readonly IAdditionPerformer performer;
+        private readonly ISubtractionPerformer subtractionPerformer;
 
-        public CalculatorService(IAdditionPerformer performer) {
+        public CalculatorService(
+            IAdditionPerformer performer,
+            ISubtractionPerformer subtractionPerformer
+        ) {
             this.performer = performer;
+            this.subtractionPerformer = subtractionPerformer;
         }
 
         public int Add(int num1, int num2) {
             return performer.Perform(num1, num2);
+        }
+
+        public int Subtract(int num1, int num2) {
+            return subtractionPerformer.Perform(num1, num2);
         }
     }
 }
