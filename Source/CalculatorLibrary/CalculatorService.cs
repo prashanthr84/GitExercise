93cbbd8a5453a5ea1640545fa0b93f25b6edbe78
Rao <Prashanth.Rao@philips.com>
2022-04-29 22:22:59 +0530

    Skeleton for division

--- Source/CalculatorLibrary/CalculatorService.cs
+++ Source/CalculatorLibrary/CalculatorService.cs
@@ -5,18 +5,21 @@ namespace CalculatorLibrary
     public class CalculatorService
     {
         private readonly IAdditionPerformer performer;
-
         private readonly ISubtractionPerformer subtractionPerformer;
         private readonly IMultiplicationPerformer multiplicationPerformer;
+        private readonly IDivisionPerformer divisionPerformer;
+
 
         public CalculatorService(
             IAdditionPerformer performer,
             ISubtractionPerformer subtractionPerformer,
-            IMultiplicationPerformer multiplicationPerformer
+            IMultiplicationPerformer multiplicationPerformer,
+            IDivisionPerformer divisionPerformer
         ) {
             this.performer = performer;
             this.subtractionPerformer = subtractionPerformer;
             this.multiplicationPerformer = multiplicationPerformer;
+            this.divisionPerformer = divisionPerformer;
         }
 
         public int Add(int num1, int num2) {
@@ -30,6 +33,10 @@ namespace CalculatorLibrary
 
         public int Subtract(int num1, int num2) {
             return subtractionPerformer.Perform(num1, num2);
+        }
+
+        public int Divide(int num1, int num2) {
+            return divisionPerformer.Perform(num1, num2);
         }
     }
 }
