f980da819ae8a593f1435365f48c34567e83c3bc
prashanthr <prashanthr84@gmail.com>
2022-04-29 22:31:52 +0530

    Enable skeleton for division and some minor refactoring (#7)
    Supports division

--- Source/CalculatorLibrary/CalculatorService.cs
+++ Source/CalculatorLibrary/CalculatorService.cs
@@ -4,23 +4,40 @@ namespace CalculatorLibrary
 {
     public class CalculatorService
     {
-        private readonly IAdditionPerformer performer;
+        private readonly IAdditionPerformer additionPerformer;
 
         private readonly ISubtractionPerformer subtractionPerformer;
         private readonly IMultiplicationPerformer multiplicationPerformer;
+        private readonly IDivisionPerformer divisionPerformer;
+
 
         public CalculatorService(
-            IAdditionPerformer performer,
+            IAdditionPerformer additionPerformer,
             ISubtractionPerformer subtractionPerformer,
-            IMultiplicationPerformer multiplicationPerformer
+            IMultiplicationPerformer multiplicationPerformer,
+            IDivisionPerformer divisionPerformer
         ) {
-            this.performer = performer;
+            this.additionPerformer = additionPerformer;
             this.subtractionPerformer = subtractionPerformer;
             this.multiplicationPerformer = multiplicationPerformer;
+            this.divisionPerformer = divisionPerformer;
         }
 
         public int Add(int num1, int num2) {
-            return performer.Perform(num1, num2);
+            return additionPerformer.Perform(num1, num2);
+        }
+
+        public int Multiply(int num1, int num2)
+        {
+            return multiplicationPerformer.Perform(num1, num2);
+        }
+
+        public int Subtract(int num1, int num2) {
+            return subtractionPerformer.Perform(num1, num2);
+        }
+
+        public int Divide(int num1, int num2) {
+            return divisionPerformer.Perform(num1, num2);
         }
 
         public int Multiply(int num1, int num2)
