2079f92091401f6e6e3047b3c0ec0853344fbf84
Rao <Prashanth.Rao@philips.com>
2022-04-29 21:47:48 +0530

    added addition performer

---
+++ Source/CalculatorLibrary/AdditionPerformer.cs
@@ -0,0 +1,7 @@
+﻿namespace CalculatorLibrary {
+    public class AdditionPerformer : IAdditionPerformer {
+        public int Perform(in int num1, in int num2) {
+            return num1 + num2;
+        }
+    }
+}
